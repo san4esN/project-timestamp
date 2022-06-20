d9ac8960878117b2d41adb811f33894522b90cbe
aleksandr_nikolaev <4san4es@gmail.com>
2022-06-20 18:37:02 +0600

    Time done

--- index.js
+++ index.js
@@ -4,7 +4,6 @@
 // init project
 var express = require('express');
 var app = express();
-
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
@@ -24,7 +23,30 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
+app.get("/api/:date?",function(req,res){
+  const dateParam = req.params.date;
+  let reqDate;
+  if(typeof(dateParam) === "undefined"){
+    reqDate = new Date(Date.now());
+  }
+  else{
+    const check = new RegExp(/[^\d]/).test(dateParam);
+    if(check)
+    {
+      reqDate = new Date(dateParam);
+    }
+    else
+    {
+      reqDate = new Date(parseInt(dateParam))
+    }
+  }
+  if(reqDate instanceof Date && !isNaN(reqDate)){ //checks if reqDate is a Date object and try to cast it to a number(timestamp) isNan can be replaced with isFinite. 
+    res.json({"unix":reqDate.getTime(), "utc":reqDate.toUTCString()})
+  }
+  else{
+    res.json({"error": "Invalid Date"})
+  }
+})
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
